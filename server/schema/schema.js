b7360645f3385363755f2da27cc77fa8a301f182
Rowaa <rowaa.almogheer@gmail.com>
2021-03-13 22:12:36 +0100

    build schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,31 @@
 const graphql = require('graphql');
 
+const{GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: ()=> ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString},
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    field: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //code to get data from db/ other source
+                
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
+
