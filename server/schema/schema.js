4255ef8dc944b3290e0cc206848c3d252bc6c9e3
Rowaa <rowaa.almogheer@gmail.com>
2021-03-14 11:43:10 +0100

    graphiql works

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,24 @@
 const graphql = require('graphql');
 
-const{GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const{GraphQLObjectType,
+     GraphQLString,
+      GraphQLSchema,
+      GraphQLID
+    } = graphql;
+
+const _ = require('lodash');
+
+//dummy data
+var books =[
+    {name:'Name of the wind', genre:'Fantasy', id:'1'},
+    {name:'The final Empire', genre:'Fantasy', id:'2'},
+    {name:'The Long Earth', genre:'Sci-Fi', id:'3'},
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: ()=> ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString},
     })
@@ -16,10 +29,11 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args){
                 //code to get data from db/ other source
-                
+                //console.log(typeof(args.id));
+               return _.find(books, {id:args.id}) 
             }
         }
     }
