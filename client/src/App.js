4e251b1b4d9fa425fc2f2088ddf6153fdb6eaf3a
Rowaa <rowaa.almogheer@gmail.com>
2021-03-17 13:05:25 +0100

    get the data from  server + install cros (server)

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Booklist from './components/BookList';
 
 //apollo client setup
 const client = new ApolloClient({
-  uri: 'https://localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 });
 
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,32 @@
+import {useQuery, gql} from '@apollo/client';
+
+
+const BooksQuery = gql`
+query GetBooksQuery{
+  books{
+      name
+      genre
+      
+  }  
+}
+`
+
 function BookList() {
-    return (
+    
+    const { loading, error, data } = useQuery(BooksQuery);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+        console.log(data.books);
+    return data.books.map(({name, genre})=> (
       <div>
       <ul id="book-list">
-        <li>Book name</li>
+        <li>{name} : {genre} </li>
       </ul>
       </div>
-    );
+    ));
+    
   }
   
   export default BookList;
