52e1c5dfe826f07a0ffe9b6bfba4e51fbe89fdfc
Rowaa <rowaa.almogheer@gmail.com>
2021-03-17 11:48:51 +0100

    book component + install apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 
+
+//components
+import Booklist from './components/BookList';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'https://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Book's Reading List</h1>
+        <Booklist></Booklist>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,11 @@
+function BookList() {
+    return (
+      <div>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+      </div>
+    );
+  }
+  
+  export default BookList;
